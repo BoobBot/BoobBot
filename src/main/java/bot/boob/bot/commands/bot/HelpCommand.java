3c3229f8118aa1811ce93757d2895b641956cb35
Kanin <itsthekanin@gmail.com>
2018-09-14 14:55:44 -0400

    Added PHGif command

--- src/main/java/bot/boob/bot/commands/bot/HelpCommand.java
+++ src/main/java/bot/boob/bot/commands/bot/HelpCommand.java
@@ -79,6 +79,7 @@ public class HelpCommand implements Command {
                          + "**{0}gif**: Gifs!!!\n"
                          + "**{0}hentai**: Hentai.\n"
                          + "**{0}pegged**: Strap-on love!\n"
+                         + "**{0}phgif**: PornHub gifs! (Some are trash #BlamePornHub)\n"
 						 + "**{0}clean**: Deletes all bot messages and triggers.\n"
                          + "**{0}nsfw**: Toggles the current channels nsfw setting.\n"
                          + "**{0}invite**: bot, support server and paypal links.\n",
