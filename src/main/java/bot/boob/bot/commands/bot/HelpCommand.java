18717d90a0b422967ce7f51188bda16c0bc74506
Kanin <itsthekanin@gmail.com>
2018-09-14 16:59:04 -0400

    Added Vday and Xmas commands

--- src/main/java/bot/boob/bot/commands/bot/HelpCommand.java
+++ src/main/java/bot/boob/bot/commands/bot/HelpCommand.java
@@ -101,6 +101,8 @@ public class HelpCommand implements Command {
                          + "**{0}poke**: Pokemon Porn!\n"
                          + "**{0}real**: Real girls!\n"
                          + "**{0}toys**: Everything is better with toys \uD83D\uDE09\n"
+                         + "**{0}vday**: Valentines ❤\n"
+                         + "**{0}xmas**: Christmas \uD83C\uDF85\n"
                          + "**{0}clean**: Deletes all bot messages and triggers.\n"
                          + "**{0}nsfw**: Toggles the current channels nsfw setting.\n"
                          + "**{0}invite**: bot, support server and PayPal links.\n",
