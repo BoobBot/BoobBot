098dc790bb5297631707c6db7b1c0ebb379280c6
Tom <hydrabott@gmail.com>
2018-09-15 13:32:32 -0400

    rt

--- src/main/java/bot/boob/bot/commons/apis/RTapi.java
+++ src/main/java/bot/boob/bot/commons/apis/RTapi.java
@@ -1,5 +1,6 @@
 package bot.boob.bot.commons.apis;
 
+import bot.boob.bot.commons.Misc;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -9,13 +10,16 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.Objects;
 
-public class phapi {
+public class RTapi {
     private static final OkHttpClient client = new OkHttpClient();
 
     public static JSONObject makeReqAndGetRes(String tag) throws Exception {
+        OkHttpClient rtClient = client.newBuilder()
+                .proxy(Misc.getProxy())
+                .build();
         Request req = new Request.Builder().url("https://api.redtube.com/?data=redtube.Videos.searchVideos&output=json&search="+tag+"&thumbsize=big&ordering=mostviewed&page=1")
                 .build();
-        Response res = client.newCall(req).execute();
+        Response res = rtClient.newCall(req).execute();
         try (ResponseBody responseBody = res.body()) {
             if(!res.isSuccessful()) throw new IOException("shit, req failed with "+res+" wsi down again?");
             return new JSONObject(Objects.requireNonNull(responseBody).string());
