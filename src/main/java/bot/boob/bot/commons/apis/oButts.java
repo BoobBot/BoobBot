d40ec2c9ff9d67b85f8e0f0b27608da19236203f
Tom <hydrabott@gmail.com>
2018-09-16 14:55:38 -0400

    obutts/oBoobs.ru

---
+++ src/main/java/bot/boob/bot/commons/apis/oButts.java
@@ -0,0 +1,31 @@
+package bot.boob.bot.commons.apis;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class oButts {
+    private static final OkHttpClient client = new OkHttpClient();
+
+    public static String getAss() throws Exception {
+        Request request = new Request.Builder()
+                .url("http://api.obutts.ru/butts/0/1/random")
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response);
+        try (ResponseBody responseBody = response.body()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            return "http://media.obutts.ru/" + new JSONObject(Objects.requireNonNull(responseBody).string()
+                    .replace("[","")
+                    .replace("]",""))
+                    .get("preview")
+                    .toString();
+        }
+    }
+
+}
