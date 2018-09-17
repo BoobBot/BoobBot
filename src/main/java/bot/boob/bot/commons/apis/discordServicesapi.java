313c0acdfa2be95ac9ff4f1513850c8c91907bc7
Devoxin <lukel9914@gmail.com>
2018-09-17 20:36:43 +0100

    Clean slate

--- src/main/java/bot/boob/bot/commons/apis/discordServicesapi.java
+++
@@ -1,9 +0,0 @@
-package bot.boob.bot.commons.apis;
-
-import java.net.URL;
-
-public class discordServicesapi {
-    public static URL getMagikUrl(String url) throws Exception {
-        return new URL("https://discord.services/api/magik/?url=" + url);
-    }
-}
