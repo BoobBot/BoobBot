3513b57aa87bc0397be60d47ce2124fc18446b84
Devoxin <lukel9914@gmail.com>
2018-09-17 21:46:19 +0100

    Main stuff

---
+++ src/main/java/bot/boobbot/commons/Constants.java
@@ -0,0 +1,24 @@
+package bot.boobbot.commons;
+
+public class Constants {
+    public static final long[] OWNERS = {
+            248294452307689473L, 95645231248048128L, 472573259108319237L, 173237945149423619L
+    };
+
+    public static String TOKEN = "Mjg1NDgwNDI0OTA0MzI3MTc5.DoBbBw.F_dXWInj91wvn2aU0sXtlJ9bY1s";
+
+    public static final String DEBUG_TOKEN =
+            "NDg5MDU2MDc3ODU0NTM5Nzgy.DnlMwg.JGEL9ARaxh0lHDOmX8_kFyjbEWA";
+
+    public static final Long HOME_GUILD = 440526421388165120L;
+
+    public static final String RDY_WEBHOOK =
+            "https://discordapp.com/api/webhooks/490218285493780480/SiwYX3k3UM7QamIgFMa7yS_aEa2H3DEvnEGD4M5su3yG6nPlaHUZ0pOxyaG_3NWP3L5B";
+
+    public static final String GJLOG_WEBHOOK =
+            "https://discordapp.com/api/webhooks/490218180652957716/dzgFWnc8jMAj-vJvpACFzMooljgOyNKwgdLQWOR7XTihmAzEKKbWbHKK7F1eot0ozBvT";
+
+    public static final String GLLOG_WEBHOOK =
+            "https://discordapp.com/api/webhooks/490218180652957716/dzgFWnc8jMAj-vJvpACFzMooljgOyNKwgdLQWOR7XTihmAzEKKbWbHKK7F1eot0ozBvT";
+
+}
