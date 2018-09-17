2efc87673ec308d50ad083c9cf072562216212b0
Devoxin <lukel9914@gmail.com>
2018-09-17 22:07:28 +0100

    Properties function

--- src/main/java/bot/boobbot/flight/Command.java
+++ src/main/java/bot/boobbot/flight/Command.java
@@ -6,6 +6,8 @@ public interface Command {
 
     public void execute(MessageReceivedEvent event, String[] args);
 
-
+    default public CommandProperties getProperties() {
+        return this.getClass().getAnnotation(CommandProperties.class);
+    }
 
 }
