3475ee6b7ffa90883d28ffcecdd4029d5a68e14e
Devoxin <lukel9914@gmail.com>
2018-09-18 19:53:26 +0100

    Magik stuff is back again

---
+++ src/main/java/bot/boobbot/commands/nsfw/Magik.kt
@@ -0,0 +1,71 @@
+package bot.boobbot.commands.nsfw
+
+import bot.boobbot.BoobBot
+import bot.boobbot.flight.AsyncCommand
+import bot.boobbot.flight.CommandProperties
+import bot.boobbot.flight.Context
+import bot.boobbot.misc.*
+import java.awt.Color
+import java.net.URLEncoder
+
+@CommandProperties(description = "Super freaky porn")
+class Magik : AsyncCommand {
+
+    override suspend fun executeAsync(ctx: Context) {
+        if (ctx.args.isEmpty()) {
+            return ctx.embed {
+                setColor(Color.red)
+                setDescription(Formats.error("Missing Args\nbbmagik <type>\nTypes: boobs, ass, dick"))
+            }
+        }
+
+        val url: String
+
+        when (ctx.args[0]) {
+            "boobs" -> {
+                val oboobs = BoobBot.getRequestUtil()
+                        .get("http://api.oboobs.ru/boobs/0/1/random").await()?.jsonArray()
+                        ?: return ctx.send("rip some error, press f")
+
+                url = "http://media.oboobs.ru/" + oboobs.getJSONObject(0).getString("preview")
+            }
+            "ass" -> {
+                val obutts = BoobBot.getRequestUtil()
+                        .get("http://api.obutts.ru/butts/0/1/random").await()?.jsonArray()
+                        ?: return ctx.send("rip some error, press f")
+
+                url = "http://media.obutts.ru/" + obutts.getJSONObject(0).getString("preview")
+            }
+            "dick" -> {
+                val headers = createHeaders(
+                        Pair("Key", Constants.BB_API_KEY)
+                )
+
+                val bb = BoobBot.getRequestUtil()
+                        .get("https://boob.bot/api/v2/img/penis", headers).await()?.json()
+                        ?: return ctx.send("rip some error, press f")
+
+                url = bb.getString("url")
+            }
+            else -> {
+                return ctx.embed {
+                    setColor(Color.red)
+                    setDescription(Formats.error("What?\nTypes: boobs, ass, dick"))
+                }
+            }
+        }
+
+        val res = BoobBot.getRequestUtil()
+                .get(
+                        "https://dankmemer.services/api/magik?avatar1=${URLEncoder.encode(url, Charsets.UTF_8.name())}",
+                        createHeaders(Pair("Authorization", Constants.MEMER_IMGEN_KEY))
+                )
+                .await()
+                ?: return ctx.send("rip some error, press f")
+
+        val body = res.body() ?: return ctx.send("rip some error, press f")
+
+        ctx.channel.sendFile(body.byteStream(), "magik.png").queue()
+    }
+
+}
