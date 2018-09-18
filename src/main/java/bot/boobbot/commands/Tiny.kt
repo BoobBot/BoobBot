54ff378d7b84b9baa12b94b6b143a714db081ebb
Devoxin <lukel9914@gmail.com>
2018-09-18 16:21:12 +0100

    Bunch of commands

--- src/main/java/bot/boobbot/commands/Tiny.kt
+++ src/main/java/bot/boobbot/commands/Tiny.kt
@@ -3,5 +3,5 @@ package bot.boobbot.commands
 import bot.boobbot.flight.CommandProperties
 import bot.boobbot.models.BbApiCommand
 
-@CommandProperties(description = "Play nice.", nsfw = true)
-class Collared : BbApiCommand("collared") // a
+@CommandProperties(description = "Tiny girls!", nsfw = true)
+class Tiny : BbApiCommand("tiny")
--- src/main/java/bot/boobbot/commands/VDay.kt
+++ src/main/java/bot/boobbot/commands/VDay.kt
@@ -3,5 +3,5 @@ package bot.boobbot.commands
 import bot.boobbot.flight.CommandProperties
 import bot.boobbot.models.BbApiCommand
 
-@CommandProperties(description = "Play nice.", nsfw = true)
-class Collared : BbApiCommand("collared") // a
+@CommandProperties(description = "Valentines ❤", nsfw = true)
+class VDay : BbApiCommand("vday")
