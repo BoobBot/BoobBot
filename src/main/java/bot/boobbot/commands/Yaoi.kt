e914690371e56b22f360dedd6fba9a26977af73e
Devoxin <lukel9914@gmail.com>
2018-09-18 16:21:51 +0100

    last command for now

--- src/main/java/bot/boobbot/commands/Yaoi.kt
+++ src/main/java/bot/boobbot/commands/Yaoi.kt
@@ -3,5 +3,5 @@ package bot.boobbot.commands
 import bot.boobbot.flight.CommandProperties
 import bot.boobbot.models.BbApiCommand
 
-@CommandProperties(description = "Play nice.", nsfw = true)
-class Collared : BbApiCommand("collared")
+@CommandProperties(description = "Boy love.", nsfw = true)
+class Yaoi : BbApiCommand("yaoi")
