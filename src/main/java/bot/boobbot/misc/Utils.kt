7d46e93c7175d06f39c1f86f50404933bdd6f2e8
Tom <hydrabott@gmail.com>
2018-09-19 19:23:12 -0400

    rnd proxy support

--- src/main/java/bot/boobbot/misc/Utils.kt
+++ src/main/java/bot/boobbot/misc/Utils.kt
@@ -11,8 +11,27 @@ import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
 import java.io.IOException
 import java.util.Arrays
+import java.net.InetSocketAddress
+import org.json.HTTP
+import java.util.Random
+import com.google.common.collect.Lists
+import java.net.Proxy
+
 
 object Utils {
+
+    fun getProxy(): Proxy {
+        val ps = Lists.newArrayList(
+                "5.231.237.168:3213",
+                "94.249.224.97:2543",
+                "185.164.57.91:4012",
+                "185.164.57.144:9749",
+                "185.164.57.70:5756")
+        val rand = Random()
+        val proxy = ps[rand.nextInt(ps.size)]
+        val parts = proxy.split(":".toRegex(), 2).toTypedArray()
+        return Proxy(Proxy.Type.HTTP, InetSocketAddress(parts[0], Integer.parseInt(parts[1])))
+    }
 
 
     fun isDonor(user: User): Boolean {
