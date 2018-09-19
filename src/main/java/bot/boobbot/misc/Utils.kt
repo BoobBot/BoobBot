48f97f0d2d00eb7cf8f43ce5e501cad2d6e61e3e
Devoxin <lukel9914@gmail.com>
2018-09-19 16:49:53 +0100

    java->kotlin cleanup pt 1

--- src/main/java/bot/boobbot/misc/Utils.kt
+++ src/main/java/bot/boobbot/misc/Utils.kt
@@ -2,51 +2,37 @@ package bot.boobbot.misc
 
 import bot.boobbot.BoobBot
 import bot.boobbot.flight.Command
-import net.dv8tion.jda.core.entities.Member
 import net.dv8tion.jda.core.entities.User
 import okhttp3.Headers
-import okhttp3.Response
-
-import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
 import java.io.IOException
-import java.util.Arrays
-
-object Utils {
+import javax.imageio.ImageIO
 
 
-    fun isDonor(user: User): Boolean {
-        val member = user.jda.asBot().shardManager.getGuildById(Constants.HOME_GUILD).getMember(user)
-        return member?.roles?.parallelStream()?.anyMatch { x -> x.idLong == 440542799658483713L } ?: false
-    }
-
-
-    fun getCommand(commandName: String): Command? {
-        val commands = BoobBot.getCommands()
-
-        return if (commands.containsKey(commandName)) {
-            commands[commandName]
-        } else commands.values
-                .stream()
-                .filter { c -> Arrays.asList<String>(*c.properties.aliases).contains(commandName) }
-                .findFirst()
-                .orElse(null)
-
-    }
-
-    fun downloadAvatar(url: String): BufferedImage? {
-        val res = BoobBot.getRequestUtil().get(url, Headers.of()).block()
-
-        if (res?.body() == null) {
-            return null
+class Utils {
+    companion object {
+        fun isDonor(user: User): Boolean {
+            val member = BoobBot.home?.getMember(user) ?: return false
+            return member.roles.any { r -> r.idLong == 440542799658483713L }
         }
 
-        return try {
-            ImageIO.read(res.body()!!.byteStream())
-        } catch (e: IOException) {
-            null
+
+        fun getCommand(commandName: String): Command? {
+            val commands = BoobBot.getCommands()
+            return commands[commandName]
+                    ?: commands.values.firstOrNull { it.properties.aliases.contains(commandName) }
+
         }
 
-    }
+        fun downloadAvatar(url: String): BufferedImage? {
+            val body = BoobBot.requestUtil.get(url, Headers.of()).block()?.body()
+                    ?: return null
 
+            return try {
+                ImageIO.read(body.byteStream())
+            } catch (e: IOException) {
+                null
+            }
+        }
+    }
 }
