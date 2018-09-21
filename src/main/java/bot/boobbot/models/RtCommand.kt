27dde9baf9393319d826346684307b99cd5fe4d0
Devoxin <lukel9914@gmail.com>
2018-09-21 18:05:45 +0100

    Cleanup music stuff

--- src/main/java/bot/boobbot/models/RtCommand.kt
+++ src/main/java/bot/boobbot/models/RtCommand.kt
@@ -20,12 +20,14 @@ abstract class RtCommand() : AsyncCommand {
                 setDescription(Formats.error("Missing Args\nbbrt <tag> or random\n"))
             }
         }
+
         val rt = BoobBot.requestUtil.get(
                 "https://api.redtube.com/?data=redtube.Videos.searchVideos&output=json&search=" +
                         (if (ctx.args[0].toLowerCase() != "random") ctx.args[0].toLowerCase() else Formats.tag[Random().nextInt(Formats.tag.size)]) +
                         "&thumbsize=big&ordering=mostviewed&page=1",
                 useProxy = true).await()?.json()
                 ?: return ctx.send("\uD83D\uDEAB oh? something broken af")
+
         val video = rt.getJSONArray("videos").getJSONObject(0).getJSONObject("video")
         ctx.embed {
             setAuthor("RedTube video search",
