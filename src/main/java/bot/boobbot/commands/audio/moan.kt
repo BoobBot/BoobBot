99894099eea2e583c29b0ea84c27a1414d4077c1
Tom <hydrabott@gmail.com>
2018-09-23 18:28:43 -0400

    moan stuff

---
+++ src/main/java/bot/boobbot/commands/audio/moan.kt
@@ -0,0 +1,24 @@
+package bot.boobbot.commands.audio
+
+import bot.boobbot.BoobBot.Companion.getMusicManager
+import bot.boobbot.BoobBot.Companion.playerManager
+import bot.boobbot.flight.Command
+import bot.boobbot.flight.CommandProperties
+import bot.boobbot.flight.Context
+import bot.boobbot.misc.AudioLoader
+import bot.boobbot.misc.Utils.Companion.connectToVoiceChannel
+
+import bot.boobbot.misc.Utils.Companion.getRandomMoan
+
+@CommandProperties(description = "moans")
+class moan : Command {
+
+
+    override fun execute(ctx: Context) {
+        val musicManager = getMusicManager(ctx.message.guild)
+        connectToVoiceChannel(ctx.message)
+        playerManager.loadItemOrdered(musicManager, getRandomMoan().toString(), AudioLoader(musicManager, ctx))
+
+    }
+
+}
