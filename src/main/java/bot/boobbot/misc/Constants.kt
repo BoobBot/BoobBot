c4ca6b5b16f235e8ad2cbbf2088ea5edf1b18efc
Tom <hydrabott@gmail.com>
2018-09-24 08:54:49 -0400

    use env file cuz gud

--- src/main/java/bot/boobbot/misc/Constants.kt
+++ src/main/java/bot/boobbot/misc/Constants.kt
@@ -1,29 +1,33 @@
 package bot.boobbot.misc
 
 import java.util.*
+import io.github.cdimascio.dotenv.dotenv
+
 
 class Constants {
     companion object {
+        val dotenv = dotenv()
+
         val OWNERS: List<Long> = Arrays.asList(
                 248294452307689473L, 95645231248048128L, 472573259108319237L, 173237945149423619L
         )
 
-        var TOKEN = System.getenv("TOKEN") ?: ""
+        var TOKEN =  dotenv["MY_ENV_VAR1TOKEN"] ?: ""
 
-        val DEBUG_TOKEN = System.getenv("DEBUG_TOKEN") ?: ""
+        val DEBUG_TOKEN =  dotenv["DEBUG_TOKEN"] ?: ""
 
         const val HOME_GUILD = 440526421388165120L
 
-        var RDY_WEBHOOK = System.getenv("RDY_WEBHOOK") ?: "B"
+        var RDY_WEBHOOK = dotenv["RDY_WEBHOOK"] ?: ""
 
-        var GJLOG_WEBHOOK = System.getenv("GJLOG_WEBHOOK") ?: ""
+        var GJLOG_WEBHOOK = dotenv["GJLOG_WEBHOOK"] ?: ""
 
-        var GLLOG_WEBHOOK = System.getenv("GLLOG_WEBHOOK") ?: ""
+        var GLLOG_WEBHOOK = dotenv["GLLOG_WEBHOOK"] ?: ""
 
-        var BB_API_KEY = System.getenv("BB_API_KEY") ?: ""
+        var BB_API_KEY = dotenv["BB_API_KEY"] ?: ""
 
-        var MEMER_IMGEN_KEY = System.getenv("MEMER_IMGEN_KEY") ?: ""
+        var MEMER_IMGEN_KEY = dotenv["MEMER_IMGEN_KEY"] ?: ""
 
-        var SENTRY_DSN = System.getenv("SENTRY_DSN") ?: ""
+        var SENTRY_DSN = dotenv["SENTRY_DSN"] ?: ""
     }
 }
