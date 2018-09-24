c3d57da18d7c1e08a052a0c0b153f88f5cc48ac2
Tom <hydrabott@gmail.com>
2018-09-24 09:14:53 -0400

    env stuff

--- src/main/java/bot/boobbot/misc/Constants.kt
+++ src/main/java/bot/boobbot/misc/Constants.kt
@@ -1,33 +1,33 @@
 package bot.boobbot.misc
 
+import bot.boobbot.BoobBot
 import java.util.*
 import io.github.cdimascio.dotenv.dotenv
 
 
 class Constants {
     companion object {
-        val dotenv = dotenv()
-
         val OWNERS: List<Long> = Arrays.asList(
                 248294452307689473L, 95645231248048128L, 472573259108319237L, 173237945149423619L
         )
 
-        var TOKEN =  dotenv["MY_ENV_VAR1TOKEN"] ?: ""
+        var TOKEN =  BoobBot.dotenv["MY_ENV_VAR1TOKEN"] ?: ""
 
-        val DEBUG_TOKEN =  dotenv["DEBUG_TOKEN"] ?: ""
+        val DEBUG_TOKEN =  BoobBot.dotenv["DEBUG_TOKEN"] ?: ""
 
         const val HOME_GUILD = 440526421388165120L
 
-        var RDY_WEBHOOK = dotenv["RDY_WEBHOOK"] ?: ""
+        var RDY_WEBHOOK = BoobBot.dotenv["RDY_WEBHOOK"] ?: ""
 
-        var GJLOG_WEBHOOK = dotenv["GJLOG_WEBHOOK"] ?: ""
+        var GJLOG_WEBHOOK = BoobBot.dotenv["GJLOG_WEBHOOK"] ?: ""
 
-        var GLLOG_WEBHOOK = dotenv["GLLOG_WEBHOOK"] ?: ""
+        var GLLOG_WEBHOOK = BoobBot.dotenv["GLLOG_WEBHOOK"] ?: ""
 
-        var BB_API_KEY = dotenv["BB_API_KEY"] ?: ""
+        var BB_API_KEY = BoobBot.dotenv["BB_API_KEY"] ?: ""
 
-        var MEMER_IMGEN_KEY = dotenv["MEMER_IMGEN_KEY"] ?: ""
+        var MEMER_IMGEN_KEY = BoobBot.dotenv["MEMER_IMGEN_KEY"] ?: ""
 
-        var SENTRY_DSN = dotenv["SENTRY_DSN"] ?: ""
+        var SENTRY_DSN = BoobBot.dotenv["SENTRY_DSN"] ?: ""
+
     }
 }
