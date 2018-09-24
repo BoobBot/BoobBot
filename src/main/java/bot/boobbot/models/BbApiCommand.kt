025a345233cd9658b98064341e7c98693a2cfaea
Tom <hydrabott@gmail.com>
2018-09-24 18:58:18 -0400

    debug prints

--- src/main/java/bot/boobbot/models/BbApiCommand.kt
+++ src/main/java/bot/boobbot/models/BbApiCommand.kt
@@ -7,15 +7,15 @@ import bot.boobbot.misc.*
 import java.time.Instant
 
 abstract class BbApiCommand(private val category: String) : AsyncCommand {
-
     override suspend fun executeAsync(ctx: Context) {
+        BoobBot.log.info("i made it here")
         val headers = createHeaders(
                 Pair("Key", Constants.BB_API_KEY)
         )
 
         val res = BoobBot.requestUtil.get("https://boob.bot/api/v2/img/$category", headers).await()?.json()
                 ?: return ctx.send("\uD83D\uDEAB oh? something broken af")
-
+        BoobBot.log.info("and here")
         ctx.embed {
             setDescription(Formats.LEWD_EMOTE)
             setColor(Colors.getEffectiveColor(ctx.message))
@@ -23,6 +23,6 @@ abstract class BbApiCommand(private val category: String) : AsyncCommand {
             setFooter("Requested by ${ctx.author.name}", ctx.author.effectiveAvatarUrl)
             setTimestamp(Instant.now())
         }
-
+        BoobBot.log.info("also here")
     }
 }
