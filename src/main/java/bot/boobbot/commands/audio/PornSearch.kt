ad1e98b16737892d9481101a334c5d2324c2a6b6
Devoxin <lukel9914@gmail.com>
2018-10-06 22:25:18 +0100

    Porn -> PornSearch, add play command

---
+++ src/main/java/bot/boobbot/commands/audio/PornSearch.kt
@@ -0,0 +1,25 @@
+package bot.boobbot.commands.audio
+
+import bot.boobbot.BoobBot.Companion.playerManager
+import bot.boobbot.audio.AudioLoader
+import bot.boobbot.flight.CommandProperties
+import bot.boobbot.flight.Context
+import bot.boobbot.models.VoiceCommand
+
+@CommandProperties(description = "Searches PornHub for videos to play")
+class PornSearch : VoiceCommand {
+
+    override fun execute(ctx: Context) {
+        val shouldPlay = performVoiceChecks(ctx)
+
+        if (!shouldPlay) {
+            return
+        }
+
+        val player = ctx.audioPlayer!!
+        val query = "phsearch:${ctx.args.joinToString(" ")}"
+
+        playerManager.loadItem(query, AudioLoader(player, ctx))
+    }
+
+}
