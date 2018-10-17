3d521955ea7e269b3754cf1a3c35c6f12d8cd397
Tomsusername <hydrabott@gmail.com>
2018-10-17 09:52:40 -0400

    why, facts

---
+++ src/main/kotlin/bot/boobbot/commands/fun/Why.kt
@@ -0,0 +1,23 @@
+package bot.boobbot.commands.`fun`
+
+import bot.boobbot.BoobBot
+import bot.boobbot.flight.AsyncCommand
+import bot.boobbot.flight.Category
+import bot.boobbot.flight.CommandProperties
+import bot.boobbot.flight.Context
+import bot.boobbot.misc.json
+
+@CommandProperties(description = "Random why questions", category = Category.FUN)
+class Why : AsyncCommand {
+
+    override suspend fun executeAsync(ctx: Context) {
+
+        val res = BoobBot.requestUtil
+                .get("https://nekos.life/api/v2/why")
+                .await()
+                ?: return ctx.send("rip some error, press f")
+        val body = res.json() ?: return ctx.send("rip some error, press f")
+        ctx.send(body.get("why").toString())
+    }
+
+}
