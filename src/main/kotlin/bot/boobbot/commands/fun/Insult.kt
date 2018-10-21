351ff462f68e693a41a3655ddbaf60b66cb8d4d4
iamtails <hydrabott@gmail.com>
2018-10-21 17:31:39 -0400

    overlapping alias

--- src/main/kotlin/bot/boobbot/commands/fun/Insult.kt
+++ src/main/kotlin/bot/boobbot/commands/fun/Insult.kt
@@ -4,5 +4,5 @@ import bot.boobbot.flight.Category
 import bot.boobbot.flight.CommandProperties
 import bot.boobbot.models.FunCommand
 
-@CommandProperties(description = "Insult someone.", category = Category.FUN, aliases = ["pu"])
+@CommandProperties(description = "Insult someone.", category = Category.FUN, aliases = ["ins"])
 class Insult : FunCommand("insult")
