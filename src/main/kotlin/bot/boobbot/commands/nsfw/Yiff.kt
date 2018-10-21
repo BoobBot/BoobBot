ba93eaf5a9067cae9e8ed5052a98a1b0952b6a1f
iamtails <Nympho69999>
2018-10-21 18:16:14 -0400

    fucking furries

---
+++ src/main/kotlin/bot/boobbot/commands/nsfw/Yiff.kt
@@ -0,0 +1,8 @@
+package bot.boobbot.commands.nsfw
+
+import bot.boobbot.flight.Category
+import bot.boobbot.flight.CommandProperties
+import bot.boobbot.models.BbApiCommand
+
+@CommandProperties(description = "Fucking furries", nsfw = true, category = Category.FANTASY)
+class Yiff : BbApiCommand("yiff")
