f25aa8075363ae104a34ef3b509edf723aeb1d41
Tails <hydrabott@gmail.com>
2019-01-26 00:30:34 -0500

    Disconnect

---
+++ src/main/kotlin/bot/boobbot/commands/audio/Disconnect.kt
@@ -0,0 +1,34 @@
+package bot.boobbot.commands.audio
+
+import bot.boobbot.flight.Category
+import bot.boobbot.flight.CommandProperties
+import bot.boobbot.flight.Context
+import bot.boobbot.misc.Colors
+import bot.boobbot.misc.Constants
+import bot.boobbot.misc.Formats
+import bot.boobbot.models.VoiceCommand
+import net.dv8tion.jda.core.Permission
+
+@CommandProperties(
+    description = "Disconnects bot",
+    category = Category.AUDIO,
+    guildOnly = true,
+    nsfw = false
+)
+class Disconnect : VoiceCommand {
+
+    override fun execute(ctx: Context) {
+
+        val player = ctx.audioPlayer!!
+        if (
+            ctx.userCan(Permission.MESSAGE_MANAGE)
+            || Constants.OWNERS.contains(ctx.author.idLong)
+            || isDJ(ctx.member!!))
+        {
+            player.shutdown()
+            return ctx.send(Formats.info("Done, Whore"))
+        }
+        return ctx.send(Formats.error("No whore, i can't let you do that"))
+    }
+
+}
