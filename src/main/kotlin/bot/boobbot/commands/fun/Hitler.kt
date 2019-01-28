9b773deed2688e3e44c98fce546ec25a00489b92
IamTails <hydrabott@gmail.com>
2019-01-28 18:41:58 -0500

    more

---
+++ src/main/kotlin/bot/boobbot/commands/fun/Hitler.kt
@@ -0,0 +1,8 @@
+package bot.boobbot.commands.fun
+
+import bot.boobbot.flight.Category
+import bot.boobbot.flight.CommandProperties
+import bot.boobbot.models.MemeAvatarCommand
+
+@CommandProperties(description = "Hitler.", nsfw = false, category = Category.FUN, guildOnly = true)
+class Hitler : MemeAvatarCommand("hitler")
