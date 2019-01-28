363d7f879c65c078fde357749f95f1175e086c01
IamTails <hydrabott@gmail.com>
2019-01-28 17:01:30 -0500

    Thanks mel :tip:

---
+++ src/main/kotlin/bot/boobbot/commands/fun/Failure.kt
@@ -0,0 +1,38 @@
+package bot.boobbot.commands.`fun`
+
+import bot.boobbot.BoobBot
+import bot.boobbot.flight.*
+import bot.boobbot.misc.Constants
+
+import bot.boobbot.misc.createHeaders
+import net.dv8tion.jda.core.entities.Member
+
+import java.net.URLEncoder
+
+@CommandProperties(description = "Failure.", nsfw = false, category = Category.FUN, guildOnly = true)
+class Failure : AsyncCommand {
+
+    override suspend fun executeAsync(ctx: Context) {
+        val user: Member
+        if (ctx.message.mentionedMembers.isEmpty()) {
+            user = ctx.member!!
+        }
+        else {
+            user = ctx.message.mentionedMembers.firstOrNull()!!
+        }
+
+        val res = BoobBot.requestUtil
+            .get(
+                "https://dankmemer.services/api/failure?avatar1=${URLEncoder.encode(user.user.avatarUrl, Charsets.UTF_8.name())}",
+                createHeaders(Pair("Authorization", Constants.MEMER_IMGEN_KEY))
+            )
+            .await()
+            ?: return ctx.send("rip some error, press f")
+
+
+        val body = res.body() ?: return ctx.send("rip some error, press f")
+
+        ctx.channel.sendFile(body.byteStream(), "uwu.png").queue()
+
+    }
+}
