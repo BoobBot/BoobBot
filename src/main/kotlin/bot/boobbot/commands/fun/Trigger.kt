28b036fcbf9247e5b6e07188b991ffed7e6f7153
IamTails <hydrabott@gmail.com>
2019-01-28 19:32:44 -0500

    move 2 to fun to drop below the limit

--- src/main/kotlin/bot/boobbot/commands/fun/Trigger.kt
+++ src/main/kotlin/bot/boobbot/commands/fun/Trigger.kt
@@ -1,7 +1,7 @@
-package bot.boobbot.commands.meme
+package bot.boobbot.commands.`fun`
 import bot.boobbot.flight.Category
 import bot.boobbot.flight.CommandProperties
 import bot.boobbot.models.MemeAvatarCommand
 
-@CommandProperties(description = "Trigger.", category = Category.MEME, guildOnly = true, aliases = ["triggered"])
+@CommandProperties(description = "Trigger.", category = Category.FUN, guildOnly = true, aliases = ["triggered"])
 class Trigger : MemeAvatarCommand("trigger")
--- src/main/kotlin/bot/boobbot/commands/fun/Warp.kt
+++ src/main/kotlin/bot/boobbot/commands/fun/Warp.kt
@@ -1,8 +1,8 @@
-package bot.boobbot.commands.meme
+package bot.boobbot.commands.`fun`
 
 import bot.boobbot.flight.Category
 import bot.boobbot.flight.CommandProperties
 import bot.boobbot.models.MemeAvatarCommand
 
-@CommandProperties(description = "Warp.", category = Category.MEME, guildOnly = true)
+@CommandProperties(description = "Warp.", category = Category.FUN, guildOnly = true)
 class Warp : MemeAvatarCommand("warp")
