a4a8261cb22a12c77d88571182c2f1bf2fc99fbc
Devoxin <lukel9914@gmail.com>
2019-03-25 13:55:13 +0000

    Minor changes to eventhandler

--- src/main/kotlin/bot/boobbot/handlers/EventHandler.kt
+++ src/main/kotlin/bot/boobbot/handlers/EventHandler.kt
@@ -16,6 +16,7 @@ import net.dv8tion.jda.core.events.ResumedEvent
 import net.dv8tion.jda.core.events.guild.GuildJoinEvent
 import net.dv8tion.jda.core.events.guild.GuildLeaveEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
+import net.dv8tion.jda.webhook.WebhookClient
 import net.dv8tion.jda.webhook.WebhookClientBuilder
 import net.dv8tion.jda.webhook.WebhookMessage
 import net.dv8tion.jda.webhook.WebhookMessageBuilder
@@ -44,19 +45,30 @@ class EventHandler : ListenerAdapter() {
             .build()
     }
 
+    fun safeSend(whClient: WebhookClient, message: WebhookMessage) {
+        try {
+            whClient.send(message)
+        } catch (e: Exception) {
+            BoobBot.log.error("Failed to send message to webhook", e)
+        }
+    }
+
+    fun allShardsReady(): Boolean {
+        return BoobBot.shardManager.shards.all { it.status == JDA.Status.CONNECTED || it.status == JDA.Status.LOADING_SUBSYSTEMS }
+        // LOADING_SYSTEMS = receiving info from Discord, so technically connected, and should be ready in a few seconds.
+    }
+
     override fun onReady(event: ReadyEvent) {
         BoobBot.metrics.record(Metrics.happened("Ready"))
         BoobBot.log.info("Ready on shard: ${event.jda.shardInfo.shardId}, Ping: ${event.jda.ping}ms, Status: ${event.jda.status}")
 
-        shardHook.send(
-            composeEmbed(event.jda) {
-                setTitle("SHARD READY [${event.jda.shardInfo.shardId}]", BoobBot.inviteUrl)
-                setDescription("Ping: ${event.jda.ping}") // Don't need status as it's included in title
-            }
-        )
+        safeSend(shardHook,composeEmbed(event.jda) {
+            setTitle("SHARD READY [${event.jda.shardInfo.shardId}]", BoobBot.inviteUrl)
+            setDescription("Ping: ${event.jda.ping}") // Don't need status as it's included in title
+        })
 
         // ReadyCount is a bad way of tracking, because Shards can emit ready multiple times.
-        if (BoobBot.isAllShardsConnected() && !BoobBot.isReady) {
+        if (allShardsReady() && !BoobBot.isReady) {
             BoobBot.isReady = true
             if (!BoobBot.isDebug) { // dont need this is testing
                 BoobBot.scheduler.scheduleAtFixedRate(Utils.auto(), 1, 2, TimeUnit.HOURS)
@@ -64,52 +76,43 @@ class EventHandler : ListenerAdapter() {
             BoobBot.shardManager.setPresence(OnlineStatus.ONLINE, Game.playing("bbhelp || bbinvite"))
             BoobBot.log.info(Formats.getReadyFormat())
 
-            shardHook.send(
-                composeEmbed(event.jda) {
-                    setTitle("ALL SHARDS CONNECTED", BoobBot.inviteUrl)
-                    setDescription("Average Shard Ping: ${BoobBot.shardManager.averagePing}")
-                    setThumbnail(event.jda.selfUser.effectiveAvatarUrl)
-                    addField("Ready Info", "```\n${Formats.getReadyFormat()}```", false)
-                }
-            )
+            safeSend(shardHook, composeEmbed(event.jda) {
+                setTitle("ALL SHARDS CONNECTED", BoobBot.inviteUrl)
+                setDescription("Average Shard Ping: ${BoobBot.shardManager.averagePing}")
+                setThumbnail(event.jda.selfUser.effectiveAvatarUrl)
+                addField("Ready Info", "```\n${Formats.getReadyFormat()}```", false)
+            })
         }
     }
-
 
     override fun onReconnect(event: ReconnectedEvent) {
         BoobBot.metrics.record(Metrics.happened("Reconnected"))
         BoobBot.log.info("Reconnected on shard: ${event.jda.shardInfo.shardId}, Status: ${event.jda.status}")
 
-        shardHook.send(
-            composeEmbed(event.jda) {
-                setTitle("SHARD RECONNECTED [${event.jda.shardInfo.shardId}]")
-                setDescription("Ping: ${event.jda.ping}")
-            }
-        )
+        safeSend(shardHook, composeEmbed(event.jda) {
+            setTitle("SHARD RECONNECTED [${event.jda.shardInfo.shardId}]")
+            setDescription("Ping: ${event.jda.ping}")
+        })
     }
 
     override fun onResume(event: ResumedEvent) {
         BoobBot.metrics.record(Metrics.happened("Resumed"))
         BoobBot.log.info("Resumed on shard: ${event.jda.shardInfo.shardId}, Status: ${event.jda.status}")
 
-        shardHook.send(
-            composeEmbed(event.jda) {
-                setTitle("SHARD RESUMED [${event.jda.shardInfo.shardId}]")
-                setDescription("Ping: ${event.jda.ping}")
-            }
-        )
+        safeSend(shardHook, composeEmbed(event.jda) {
+            setTitle("SHARD RESUMED [${event.jda.shardInfo.shardId}]")
+            setDescription("Ping: ${event.jda.ping}")
+        })
     }
 
     override fun onDisconnect(event: DisconnectEvent) {
         BoobBot.metrics.record(Metrics.happened("Disconnect"))
         BoobBot.log.info("Disconnect on shard: ${event.jda.shardInfo.shardId}, Status: ${event.jda.status}")
 
-        shardHook.send(
-            composeEmbed(event.jda) {
-                setTitle("SHARD DISCONNECTED [${event.jda.shardInfo.shardId}]")
-                setTitle("Ping: ${event.jda.ping}") // will probably be -1 or something lol
-            }
-        )
+        safeSend(shardHook, composeEmbed(event.jda) {
+            setTitle("SHARD DISCONNECTED [${event.jda.shardInfo.shardId}]")
+            setDescription("Ping: ${event.jda.ping}") // will probably be -1 or something lol
+        })
     }
 
     override fun onGuildJoin(event: GuildJoinEvent) {
@@ -118,39 +121,35 @@ class EventHandler : ListenerAdapter() {
 
         val guild = event.guild
 
-        joinHook.send(
-            composeEmbed(event.jda) {
-                setColor(Color.green)
-                setTitle("Guild Joined: ${guild.name}")
-                setDescription(
-                    "${Formats.info("info")}\n" +
-                            "On Shard: ${event.jda.shardInfo.shardId}\n" +
-                            "Total Guilds: ${BoobBot.shardManager.guilds.size}\n\n" +
-                            "Owner: ${guild.owner.user.asTag} (${guild.owner.user.id})\n" +
-                            "Members: ${guild.members.size}"
-                )
-                setThumbnail(guild.iconUrl)
-            }
-        )
+        safeSend(joinHook, composeEmbed(event.jda) {
+            setColor(Color.green)
+            setTitle("Guild Joined: ${guild.name}")
+            setDescription(
+                "${Formats.info("info")}\n" +
+                        "On Shard: ${event.jda.shardInfo.shardId}\n" +
+                        "Total Guilds: ${BoobBot.shardManager.guilds.size}\n\n" +
+                        "Owner: ${guild.owner.user.asTag} (${guild.owner.user.id})\n" +
+                        "Members: ${guild.members.size}"
+            )
+            setThumbnail(guild.iconUrl)
+        })
     }
 
     override fun onGuildLeave(event: GuildLeaveEvent) {
         BoobBot.metrics.record(Metrics.happened("GuildLeave"))
         val guild = event.guild
 
-        leaveHook.send(
-            composeEmbed(event.jda) {
-                setColor(Color.red)
-                setTitle("Guild Left: ${guild.name}")
-                setDescription(
-                    "${Formats.info("info")}\n" +
-                            "On Shard: ${event.jda.shardInfo.shardId}\n" +
-                            "Total Guilds: ${BoobBot.shardManager.guilds.size}\n\n" +
-                            "Owner: ${guild.owner.user.asTag} (${guild.owner.user.id})\n" +
-                            "Members: ${guild.members.size}"
-                )
-                setThumbnail(guild.iconUrl)
-            }
-        )
+        safeSend(leaveHook, composeEmbed(event.jda) {
+            setColor(Color.red)
+            setTitle("Guild Left: ${guild.name}")
+            setDescription(
+                "${Formats.info("info")}\n" +
+                        "On Shard: ${event.jda.shardInfo.shardId}\n" +
+                        "Total Guilds: ${BoobBot.shardManager.guilds.size}\n\n" +
+                        "Owner: ${guild.owner.user.asTag} (${guild.owner.user.id})\n" +
+                        "Members: ${guild.members.size}"
+            )
+            setThumbnail(guild.iconUrl)
+        })
     }
 }
