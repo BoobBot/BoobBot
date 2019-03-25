2e97b593e7c28b9d548ee8d693c799d9601d5d96
Devoxin <lukel9914@gmail.com>
2019-03-25 13:08:22 +0000

    Cleanup eventhandler

--- src/main/kotlin/bot/boobbot/handlers/EventHandler.kt
+++ src/main/kotlin/bot/boobbot/handlers/EventHandler.kt
@@ -7,6 +7,7 @@ import bot.boobbot.misc.Formats
 import bot.boobbot.misc.Utils
 import de.mxro.metrics.jre.Metrics
 import net.dv8tion.jda.core.EmbedBuilder
+import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.OnlineStatus
 import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.entities.Game
@@ -19,37 +20,50 @@ import net.dv8tion.jda.core.events.guild.GuildJoinEvent
 import net.dv8tion.jda.core.events.guild.GuildLeaveEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
 import net.dv8tion.jda.webhook.WebhookClientBuilder
+import net.dv8tion.jda.webhook.WebhookMessage
 import net.dv8tion.jda.webhook.WebhookMessageBuilder
 import org.apache.commons.lang3.StringUtils
 import java.awt.Color
+import java.time.Instant
 import java.time.Instant.now
 import java.util.concurrent.TimeUnit
 
 
 class EventHandler : ListenerAdapter() {
     var self: User? = null // just to hold self for discon webhooks
-    var readyCount = 0
+
+    private val shardHook = WebhookClientBuilder(config.readyWebhook).build()
+    private val leaveHook = WebhookClientBuilder(config.glWebhook).build()
+    private val joinHook = WebhookClientBuilder(config.gjWebhook).build()
+
+    fun composeEmbed(jda: JDA, builder: EmbedBuilder.() -> Unit): WebhookMessage {
+        return WebhookMessageBuilder()
+            .setUsername(jda.selfUser.name)
+            .setAvatarUrl(jda.selfUser.effectiveAvatarUrl)
+            .addEmbeds(
+                EmbedBuilder()
+                    .setColor(Color.magenta) // defaults, can be overridden with `.apply`
+                    .setAuthor(jda.selfUser.name, jda.selfUser.effectiveAvatarUrl, jda.selfUser.effectiveAvatarUrl)
+                    .setTimestamp(Instant.now())
+                    .apply(builder)
+                    .build()
+            )
+            .build()
+    }
+
     override fun onReady(event: ReadyEvent) {
         BoobBot.metrics.record(Metrics.happened("Ready"))
         BoobBot.log.info("Ready on shard: ${event.jda.shardInfo.shardId}, Ping: ${event.jda.ping}ms, Status: ${event.jda.status}")
-        if (!BoobBot.isReady) {readyCount++}
-        val readyClient = WebhookClientBuilder(config.readyWebhook).build()
-        readyClient.send(
-            WebhookMessageBuilder().addEmbeds(
-                EmbedBuilder().setColor(Color.magenta)
-                    .setAuthor(
-                        event.jda.selfUser.name,
-                        event.jda.selfUser.effectiveAvatarUrl,
-                        event.jda.selfUser.effectiveAvatarUrl
-                    ).setTitle(
-                        "```Ready on shard: ${event.jda.shardInfo.shardId}, Ping: ${event.jda.ping}ms, Status: ${event.jda.status}```",
-                        BoobBot.inviteUrl
-                    )
-                    .setTimestamp(now()).build()
-            ).setUsername(event.jda.selfUser.name).setAvatarUrl(event.jda.selfUser.effectiveAvatarUrl)
-                .build()
+
+        shardHook.send(
+            composeEmbed(event.jda) {
+                setTitle("SHARD READY [${event.jda.shardInfo.shardId}]", BoobBot.inviteUrl)
+                setDescription("Ping: ${event.jda.ping}") // Don't need status as it's included in title
+            }
         )
-        if (readyCount == config.shardCount && !BoobBot.isReady) {
+
+        // ReadyCount is a bad way of tracking, because Shards can emit ready multiple times.
+        if (BoobBot.isAllShardsConnected() && !BoobBot.isReady) {
             BoobBot.isReady = true
             if (!BoobBot.isDebug) { // dont need this is testing
                 BoobBot.scheduler.scheduleAtFixedRate(Utils.auto(), 1, 2, TimeUnit.HOURS)
@@ -57,188 +71,94 @@ class EventHandler : ListenerAdapter() {
             self = event.jda.selfUser // set
             BoobBot.shardManager.setPresence(OnlineStatus.ONLINE, Game.playing("bbhelp || bbinvite"))
             BoobBot.log.info(Formats.getReadyFormat())
-            readyClient.send(
-                WebhookMessageBuilder().setContent("Ready").addEmbeds(
-                    EmbedBuilder().setColor(Color.magenta)
-                        .setAuthor(
-                            event.jda.selfUser.name,
-                            event.jda.selfUser.effectiveAvatarUrl,
-                            event.jda.selfUser.effectiveAvatarUrl
-                        )
-                        .setTitle(
-                            "${event.jda.selfUser.name} Fully Ready",
-                            BoobBot.inviteUrl
-                        )
-                        .setThumbnail(event.jda.selfUser.effectiveAvatarUrl).addField(
-                            "Ready info",
-                            "``` ${Formats.getReadyFormat()}```",
-                            false
-                        )
-                        .setTimestamp(now())
-                        .build()
-                ).setUsername(event.jda.selfUser.name).setAvatarUrl(event.jda.selfUser.effectiveAvatarUrl)
-                    .build()
+
+            shardHook.send(
+                composeEmbed(event.jda) {
+                    setTitle("ALL SHARDS CONNECTED", BoobBot.inviteUrl)
+                    setDescription("Average Shard Ping: ${BoobBot.shardManager.averagePing}")
+                    setThumbnail(event.jda.selfUser.effectiveAvatarUrl)
+                    addField("Ready Info", "```\n${Formats.getReadyFormat()}```", false)
+                }
             )
-            readyClient.close()
         }
-        readyClient.close()
     }
 
 
-    override fun onReconnect(event: ReconnectedEvent?) {
+    override fun onReconnect(event: ReconnectedEvent) {
         BoobBot.metrics.record(Metrics.happened("Reconnected"))
-        BoobBot.log.info("Reconnected on shard: ${event?.jda?.shardInfo?.shardId}, Status: ${event?.jda?.status}")
-        val readyClient = WebhookClientBuilder(config.readyWebhook).build()
-        try {
-            readyClient.send(
-                WebhookMessageBuilder().addEmbeds(
-                    EmbedBuilder().setColor(Color.green)
-                        .setAuthor(
-                            self?.name,
-                            self?.effectiveAvatarUrl,
-                            self?.effectiveAvatarUrl
-                        ).setTitle("```Reconnected on shard: ${event?.jda?.shardInfo?.shardId}, Status: ${event?.jda?.status}```")
-                        .setTimestamp(now()).build()
-                ).setUsername(self?.name).setAvatarUrl(self?.effectiveAvatarUrl)
-                    .build()
-            )
-            readyClient.close()
-        } catch (ex: Exception) {
-            readyClient.close()
-            BoobBot.log.warn("error on reconnected event", ex)
-        }
+        BoobBot.log.info("Reconnected on shard: ${event.jda.shardInfo.shardId}, Status: ${event.jda.status}")
+
+        shardHook.send(
+            composeEmbed(event.jda) {
+                setTitle("SHARD RECONNECTED [${event.jda.shardInfo.shardId}]")
+                setDescription("Ping: ${event.jda.ping}")
+            }
+        )
     }
 
-    override fun onResume(event: ResumedEvent?) {
+    override fun onResume(event: ResumedEvent) {
         BoobBot.metrics.record(Metrics.happened("Resumed"))
-        BoobBot.log.info("Resumed on shard: ${event?.jda?.shardInfo?.shardId}, Status: ${event?.jda?.status}")
-        val readyClient = WebhookClientBuilder(config.readyWebhook).build()
-        try {
-            readyClient.send(
-                WebhookMessageBuilder().addEmbeds(
-                    EmbedBuilder().setColor(Color.green)
-                        .setAuthor(
-                            self?.name,
-                            self?.effectiveAvatarUrl,
-                            self?.effectiveAvatarUrl
-                        ).setTitle("```Resumed on shard: ${event?.jda?.shardInfo?.shardId}, Status: ${event?.jda?.status}```")
-                        .setTimestamp(now()).build()
-                ).setUsername(self?.name).setAvatarUrl(self?.effectiveAvatarUrl)
-                    .build()
-            )
-            readyClient.close()
-        } catch (ex: Exception) {
-            readyClient.close()
-            BoobBot.log.warn("error on resumed event", ex)
-        }
+        BoobBot.log.info("Resumed on shard: ${event.jda.shardInfo.shardId}, Status: ${event.jda.status}")
+
+        shardHook.send(
+            composeEmbed(event.jda) {
+                setTitle("SHARD RESUMED [${event.jda.shardInfo.shardId}]")
+                setDescription("Ping: ${event.jda.ping}")
+            }
+        )
     }
 
-    override fun onDisconnect(event: DisconnectEvent?) {
+    override fun onDisconnect(event: DisconnectEvent) {
         BoobBot.metrics.record(Metrics.happened("Disconnect"))
-        BoobBot.log.info("Disconnect on shard: ${event?.jda?.shardInfo?.shardId}, Status: ${event?.jda?.status}")
-        val readyClient = WebhookClientBuilder(config.readyWebhook).build()
-        try {
-            readyClient.send(
-                WebhookMessageBuilder().addEmbeds(
-                    EmbedBuilder().setColor(Color.green)
-                        .setAuthor(
-                            self?.name,
-                            self?.effectiveAvatarUrl,
-                            self?.effectiveAvatarUrl
-                        ).setTitle("```Disconnect on shard: ${event?.jda?.shardInfo?.shardId}, Status: ${event?.jda?.status}```")
-                        .setTimestamp(now()).build()
-                ).setUsername(self?.name).setAvatarUrl(self?.effectiveAvatarUrl)
-                    .build()
-            )
-            readyClient.close()
-        } catch (ex: Exception) {
-            readyClient.close()
-            BoobBot.log.warn("error on Disconnect event", ex)
-        }
+        BoobBot.log.info("Disconnect on shard: ${event.jda.shardInfo.shardId}, Status: ${event.jda.status}")
+
+        shardHook.send(
+            composeEmbed(event.jda) {
+                setTitle("SHARD DISCONNECTED [${event.jda.shardInfo.shardId}]")
+                setTitle("Ping: ${event.jda.ping}") // will probably be -1 or something lol
+            }
+        )
     }
 
     override fun onGuildJoin(event: GuildJoinEvent) {
         BoobBot.metrics.record(Metrics.happened("GuildJoin"))
-        if (!BoobBot.isReady) {
-            return
-        }
-        val guild = event.guild
-        if (!setGame) {
-            BoobBot.shardManager.setGame(Game.playing("bbhelp || bbinvite"))
-        }
-        BoobBot.log.info("New Guild Joined ${guild.name}(${guild.id})")
-        val em = EmbedBuilder()
-            .setColor(Color.green)
-            .setAuthor(guild.name, guild.iconUrl, guild.iconUrl)
-            .setTitle("Joined ${guild.name}")
-            .setThumbnail(guild.iconUrl)
-            .setDescription("Guild info")
-            .addField(
-                Formats.info("info"),
-                "**${guild.jda.shardInfo}**\n" +
-                        "Guilds: **${BoobBot.shardManager.guilds.size}**\n" +
-                        "Owner: **${guild.owner.effectiveName}**\n" +
-                        "Guild Users: **${guild.members.size}**\n",
-                false
-            )
-            .setTimestamp(now())
-            .build()
-        val guildJoinClient = WebhookClientBuilder(config.gjWebhook).build()
-        try {
-            guildJoinClient.send(
-                WebhookMessageBuilder()
-                    .addEmbeds(em)
-                    .setUsername(if (guild.name.length > 3) StringUtils.abbreviate(guild.name, 20) else "Shity name")
-                    .setAvatarUrl(guild.iconUrl)
-                    .build()
-            )
-            guildJoinClient.close()
-        } catch (ex: java.lang.Exception) {
-            guildJoinClient.close()
-            BoobBot.log.warn("error on Guild join event", ex)
-        }
+        // Don't set presence on guildJoin and leave, this is probably an easy way to hit ratelimit if someone spams.
 
+        val guild = event.guild
+
+        joinHook.send(
+            composeEmbed(event.jda) {
+                setColor(Color.green)
+                setTitle("Guild Joined: ${guild.name}")
+                setDescription(
+                    "${Formats.info("info")}\n" +
+                            "On Shard: ${event.jda.shardInfo.shardId}\n" +
+                            "Total Guilds: ${BoobBot.shardManager.guilds.size}\n\n" +
+                            "Owner: ${guild.owner.user.asTag} (${guild.owner.user.id})\n" +
+                            "Members: ${guild.members.size}"
+                )
+                setThumbnail(guild.iconUrl)
+            }
+        )
     }
 
     override fun onGuildLeave(event: GuildLeaveEvent) {
         BoobBot.metrics.record(Metrics.happened("GuildLeave"))
-        if (!BoobBot.isReady) {
-            return
-        }
         val guild = event.guild
-        if (!setGame) {
-            BoobBot.shardManager.setGame(Game.playing("bbhelp || bbinvite"))
-        }
-        BoobBot.log.info("Guild left ${guild.name}(${guild.id})")
-        val guildLeaveClient = WebhookClientBuilder(config.glWebhook).build()
-        try {
-            guildLeaveClient.send(
-                WebhookMessageBuilder()
-                    .addEmbeds(
-                        EmbedBuilder()
-                            .setColor(Color.red)
-                            .setAuthor(guild.name, guild.iconUrl, guild.iconUrl)
-                            .setTitle("Left ${guild.name}")
-                            .setThumbnail(guild.iconUrl)
-                            .setDescription("Guild info")
-                            .addField(
-                                Formats.info("info"),
-                                "**${guild.jda.shardInfo}**\n" +
-                                        "Guilds: **${BoobBot.shardManager.guilds.size}**\n" +
-                                        "Owner: **${guild.owner.effectiveName}**\n" +
-                                        "Guild Users: **${guild.members.size}**\n",
-                                false
-                            )
-                            .build()
-                    )
-                    .setUsername(if (guild.name.length > 3) StringUtils.abbreviate(guild.name, 20) else "Shity name")
-                    .setAvatarUrl(guild.iconUrl)
-                    .build()
-            )
-            guildLeaveClient.close()
-        } catch (ex: Exception) {
-            guildLeaveClient.close()
-            BoobBot.log.warn("error on Guild leave event", ex)
-        }
+
+        leaveHook.send(
+            composeEmbed(event.jda) {
+                setColor(Color.red)
+                setTitle("Guild Left: ${guild.name}")
+                setDescription(
+                    "${Formats.info("info")}\n" +
+                            "On Shard: ${event.jda.shardInfo.shardId}\n" +
+                            "Total Guilds: ${BoobBot.shardManager.guilds.size}\n\n" +
+                            "Owner: ${guild.owner.user.asTag} (${guild.owner.user.id})\n" +
+                            "Members: ${guild.members.size}"
+                )
+                setThumbnail(guild.iconUrl)
+            }
+        )
     }
 }
