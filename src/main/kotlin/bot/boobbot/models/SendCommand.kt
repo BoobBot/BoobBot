e0b2d8d8a7dc72c53055d3b27a5e448362b12e8a
Devoxin <lukel9914@gmail.com>
2019-03-27 15:21:34 +0000

    Bump timeout to 60000ms

--- src/main/kotlin/bot/boobbot/models/SendCommand.kt
+++ src/main/kotlin/bot/boobbot/models/SendCommand.kt
@@ -32,10 +32,9 @@ abstract class SendCommand(private val category: String, private val endpoint: S
         ctx.send("Good job ${ctx.author.asMention}")
 
         val res = BoobBot.waiter.waitForMessage({
-            //println("${prompt.channelId()}-${it.channelId()}")
             it.channel.id == prompt.channel.id &&
                     it.contentRaw.toLowerCase() == "y" || it.contentRaw.toLowerCase() == "n"
-        }, 5000).await() ?: return ctx.send("${user.name} didn't respond") // timeout
+        }, 60000).await() ?: return ctx.send("${user.name} didn't respond") // timeout
 
         prompt.delete().queue()
 
