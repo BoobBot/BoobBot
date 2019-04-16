169586c655d3ca7c2a3cd906971cb586b954fb6f
Tom <hydrabott@gmail.com>
2019-04-16 15:46:03 -0400

    wow how long this been a typo

--- src/main/kotlin/bot/boobbot/commands/fun/Interact.kt
+++ src/main/kotlin/bot/boobbot/commands/fun/Interact.kt
@@ -4,5 +4,5 @@ import bot.boobbot.flight.Category
 import bot.boobbot.flight.CommandProperties
 import bot.boobbot.models.FunCommand
 
-@CommandProperties(description = "fun interactions.", category = Category.FUN, aliases = ["ini"])
+@CommandProperties(description = "fun interactions.", category = Category.FUN, aliases = ["int"])
 class Interact : FunCommand("interaction")
