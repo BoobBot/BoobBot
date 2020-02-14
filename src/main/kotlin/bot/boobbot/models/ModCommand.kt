004ebd596f122b3103701e09412b7c8207d850b1
Tails <hydrabott@gmail.com>
2020-02-14 11:44:00 -0500

    unneeded

--- src/main/kotlin/bot/boobbot/models/ModCommand.kt
+++ src/main/kotlin/bot/boobbot/models/ModCommand.kt
@@ -14,29 +14,10 @@ abstract class ModCommand : Command {
             return Pair(null, null)
         }
 
-        val target = resolveTarget(ctx, userArgument[0])
+        val target = ctx.message.mentionedMembers.firstOrNull()
             ?: return Pair(null, null)
         val reason = if (reasonArgument.isEmpty()) null else reasonArgument.joinToString(" ")
 
         return Pair(target, reason)
     }
-
-    private fun resolveTarget(ctx: Context, arg: String): Member? {
-        if (ctx.guild == null) {
-            throw IllegalStateException("Cannot resolve target when guild is null!")
-        }
-        return ctx.message.mentionedMembers.firstOrNull()
-        /* return if (arg.length > 5 && arg[arg.length - 5] == '#') {
-             ctx.guild.getMemberByTag(arg)
-         } else if (arg.length > 17 && arg.toLongOrNull() != null) {
-             ctx.guild.getMemberById(arg)
-         } else if (arg.startsWith("<@") && arg.endsWith(">")) {
-             val delimiter = if (arg.contains('!')) '!' else '@'
-             val snowflake = arg.substringAfter(delimiter).substringBefore('>')
-             return ctx.guild.getMemberById(snowflake)
-         } else {
-             ctx.guild.getMembersByName(arg, false).firstOrNull()
-         }*/
-    }
-
 }
