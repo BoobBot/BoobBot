2f1247755634299f5c31df12d3b4d3c94cb078fc
Tails <hydrabott@gmail.com>
2020-02-19 02:36:50 -0500

    balance

---
+++ src/main/kotlin/bot/boobbot/commands/economy/Balance.kt
@@ -0,0 +1,32 @@
+package bot.boobbot.commands.economy
+
+import bot.boobbot.BoobBot
+import bot.boobbot.flight.Category
+import bot.boobbot.flight.Command
+import bot.boobbot.flight.CommandProperties
+import bot.boobbot.flight.Context
+import bot.boobbot.misc.Colors
+import bot.boobbot.misc.Formats
+import bot.boobbot.misc.Formats.progressPercentage
+import kotlin.math.pow
+
+
+@CommandProperties(description = "get a Profile", category = Category.ECONOMY, developerOnly = true)
+class Balance : Command {
+
+    override fun execute(ctx: Context) {
+        val user = ctx.mentions.firstOrNull() ?: ctx.author
+        val u = BoobBot.database.getUser(user.id)
+        val em = ctx.embed {
+            setColor(Colors.getEffectiveColor(ctx.message))
+            addField(
+                Formats.info("**Balance Information**"), "" +
+                        "**Current Balance**: ${u.balance}$\n" +
+                        "**Bank Balance**: ${u.bankBalance}$\n" +
+                        "**Total Assets**: ${(u.balance + u.bankBalance)}$", false
+            )
+
+        }
+
+    }
+}
--- src/main/kotlin/bot/boobbot/handlers/EconomyHandler.kt
+++ src/main/kotlin/bot/boobbot/handlers/EconomyHandler.kt
@@ -40,34 +40,33 @@ class EconomyHandler : EventListener {
 
     private fun onGuildMessageReceivedEvent(event: GuildMessageReceivedEvent) {
         val user = BoobBot.database.getUser(event.author.id)
-        user.messagesSent = user.messagesSent + 1
-        if (!user.blacklisted) {
-            if (user.inJail) {
-                user.jailRemaining = min(user.jailRemaining - 1, 0)
-                if (user.jailRemaining == 0) {
-                    user.inJail = false
+            user.messagesSent = user.messagesSent + 1
+            if (!user.blacklisted) {
+                if (user.inJail) {
+                    user.jailRemaining = min(user.jailRemaining - 1, 0)
+                    if (user.jailRemaining == 0) {
+                        user.inJail = false
+                    }
+                    BoobBot.database.setUser(user)
                 }
-                BoobBot.database.setUser(user)
-            }
-            if (event.message.textChannel.isNSFW) {
-                val tags = Formats.tag
-                val tagSize = tags.filter { event.message.contentDisplay.contains(it.toString()) }.size
-                user.lewdPoints = user.lewdPoints + min(tagSize, 5) * (user.balance / 100) * .01.toInt()
-                user.nsfwMessagesSent = user.nsfwMessagesSent + 1
-            }
-            if (user.coolDownCount >= (0..10).random()) {
-                user.coolDownCount = (0..10).random()
-                user.experience =
-                    if (user.bonusXp != null && user.bonusXp!! > 0) user.experience + 2 else user.experience + 1
-                if (user.bonusXp != null && user.bonusXp!! > 0) {
-                    user.bonusXp = user.bonusXp!! - 1
+                if (event.message.textChannel.isNSFW) {
+                    val tags = Formats.tag
+                    val tagSize = tags.filter { event.message.contentDisplay.contains(it.toString()) }.size
+                    user.lewdPoints = user.lewdPoints + min(tagSize, 5) * (user.balance / 100) * .01.toInt()
+                    user.nsfwMessagesSent = user.nsfwMessagesSent + 1
                 }
-            }
+                if (user.coolDownCount >= (0..10).random()) {
+                    user.coolDownCount = (0..10).random()
+                    user.experience =
+                        if (user.bonusXp != null && user.bonusXp!! > 0) user.experience + 2 else user.experience + 1
+                    if (user.bonusXp != null && user.bonusXp!! > 0) {
+                        user.bonusXp = user.bonusXp!! - 1
+                    }
+                }
 
             user.level = floor(0.1 * sqrt(user.experience.toDouble())).toInt()
             user.lewdLevel = calculateLewdLevel(user)
             BoobBot.database.setUser(user)
-            // }
         }
 
         val g = BoobBot.database.getGuild(event.guild.id)!!
