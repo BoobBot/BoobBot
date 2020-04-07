09f40e3a2e68bbe777dc0298007b5f51c44d2e98
Tom <hydrabott@gmail.com>
2020-04-07 11:52:37 -0400

    rank

---
+++ src/main/kotlin/bot/boobbot/commands/economy/Rank.kt
@@ -0,0 +1,30 @@
+package bot.boobbot.commands.economy
+
+import bot.boobbot.BoobBot
+import bot.boobbot.flight.Category
+import bot.boobbot.flight.Command
+import bot.boobbot.flight.CommandProperties
+import bot.boobbot.flight.Context
+import bot.boobbot.misc.Colors
+import bot.boobbot.misc.Formats
+
+
+@CommandProperties(description = "See your current rank info.", category = Category.ECONOMY)
+class Rank : Command {
+
+    override fun execute(ctx: Context) {
+        val user = ctx.mentions.firstOrNull() ?: ctx.author
+        val u = BoobBot.database.getUser(user.id)
+        ctx.embed {
+            setColor(Colors.getEffectiveColor(ctx.message))
+            addField(
+                Formats.info("**Rank Information**"), "" +
+                        "**Current Rep**: ${u.rep}$\n" +
+                        "**Current Level**: ${u.level}$\n" +
+                        "**Current Lewd Level**: ${(u.lewdLevel)}$", false
+            )
+
+        }
+
+    }
+}
