621ed7a10b7d5ce5d44f6790f0ebaded4446a0c8
Devoxin <lukel9914@gmail.com>
2020-04-08 23:58:57 +0100

    uncreative commit message

--- src/main/kotlin/bot/boobbot/commands/economy/Rep.kt
+++ src/main/kotlin/bot/boobbot/commands/economy/Rep.kt
@@ -29,22 +29,26 @@ class Rep : Command {
         if (target.idLong == ctx.author.idLong) {
             return ctx.send("aww how sad you wanna rep yourself, well fucking don't. Go find a friend whore.")
         }
+
         val author = BoobBot.database.getUser(ctx.author.id)
         val now = Instant.now()
+
         if (author.lastRep != null) {
-            val t = author.lastRep!!.plus(12, ChronoUnit.HOURS )
+            val t = author.lastRep!!.plus(12, ChronoUnit.HOURS)
             val x = t.toEpochMilli() - now.toEpochMilli()
-            if (!t.isBefore((now))) {
+            if (!t.isBefore(now)) {
                 return ctx.send("You already gave rep today whore.\nFuck off and try again in ${getRemainingCoolDown(x)}")
             }
         }
+
         author.lastRep = now
         author.save()
-        val user = BoobBot.database.getUser(target.id)
-        user.rep += 1
-        user.save()
-        val msg = "You gave ${target.asTag} a rep point, Good job! Seems you're not completely useless after all."
-        ctx.send(msg)
+
+        BoobBot.database.getUser(target.id)
+            .apply { rep += 1 }
+            .save()
+
+        ctx.send("You gave ${target.asTag} a rep point, Good job! Seems you're not completely useless after all.")
     }
 
 }
