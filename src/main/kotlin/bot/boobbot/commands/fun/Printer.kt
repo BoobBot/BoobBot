257371a08938c07d2caa91922e03e86ab234eef3
Devoxin <lukel9914@gmail.com>
2020-06-28 23:51:56 +0100

    begin printer

---
+++ src/main/kotlin/bot/boobbot/commands/fun/Printer.kt
@@ -0,0 +1,11 @@
+package bot.boobbot.commands.`fun`
+
+import bot.boobbot.BoobBot
+import bot.boobbot.entities.framework.*
+
+@CommandProperties(description = "Porn but in text", category = Category.FUN, developerOnly = true)
+class Fun : AsyncCommand {
+    override suspend fun executeAsync(ctx: Context) {
+
+    }
+}
