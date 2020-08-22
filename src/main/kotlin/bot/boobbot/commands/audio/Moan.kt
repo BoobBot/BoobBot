3565cbe174c895acdd4bc95d5e7a7af4e446e778
Tom <hydrabott@gmail.com>
2020-08-22 11:04:25 -0400

    remove moan from help

--- src/main/kotlin/bot/boobbot/commands/audio/Moan.kt
+++ src/main/kotlin/bot/boobbot/commands/audio/Moan.kt
@@ -1,16 +1,15 @@
 package bot.boobbot.commands.audio
 
-import bot.boobbot.BoobBot.Companion.getMusicManager
 import bot.boobbot.BoobBot.Companion.playerManager
 import bot.boobbot.audio.AudioLoader
 import bot.boobbot.entities.framework.Category
 import bot.boobbot.entities.framework.CommandProperties
 import bot.boobbot.entities.framework.Context
+import bot.boobbot.entities.framework.VoiceCommand
 import bot.boobbot.utils.Utils.Companion.connectToVoiceChannel
 import bot.boobbot.utils.Utils.Companion.getRandomMoan
-import bot.boobbot.entities.framework.VoiceCommand
 
-@CommandProperties(description = "moans :tired_face:", nsfw = true, category = Category.AUDIO, guildOnly = true)
+@CommandProperties(description = "moans :tired_face:", nsfw = true, category = Category.AUDIO, guildOnly = true, enabled = false)
 class Moan : VoiceCommand {
     override fun execute(ctx: Context) {
         if (!performVoiceChecks(ctx)) {
