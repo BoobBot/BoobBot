08409ba6b78d6192b1473aa0b2ef98e97098f97d
IamTails <tom@boob.bot>
2020-09-06 13:37:25 -0400

    woof

---
+++ src/main/kotlin/bot/boobbot/commands/fun/Dog.kt
@@ -0,0 +1,8 @@
+package bot.boobbot.commands.`fun`
+
+import bot.boobbot.entities.framework.Category
+import bot.boobbot.entities.framework.CommandProperties
+import bot.boobbot.entities.framework.NekoLifeApiCommand
+
+@CommandProperties(description = "random dog", category = Category.FUN, aliases = ["woof"])
+class Dog : NekoLifeApiCommand("woof")
