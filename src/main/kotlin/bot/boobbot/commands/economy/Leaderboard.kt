95f4f5a8182005eb2cd230aebebdc78c4645565a
IamTails <tom@boob.bot>
2020-09-07 14:59:22 -0400

    async lb

--- src/main/kotlin/bot/boobbot/commands/economy/Leaderboard.kt
+++ src/main/kotlin/bot/boobbot/commands/economy/Leaderboard.kt
@@ -16,88 +16,103 @@ class Leaderboard : AsyncCommand {
         sendSubcommandHelp(ctx)
     }
 
-    @SubCommand(aliases = ["richest", "$"], description = "Global economy leaderboard \uD83C\uDFC6")
+    @SubCommand(aliases = ["richest", "$"], description = "Global economy leaderboard \uD83C\uDFC6", async = true)
     suspend fun cash(ctx: Context) {
         var msg = ""
         var count = 0
-        BoobBot.database.getAllUsers().find().sort(BasicDBObject("balance", -1)).limit(15).iterator().forEach { u ->
+        BoobBot.database.getAllUsers().find().sort(BasicDBObject("balance", -1)).limit(25).iterator().forEach { u ->
             if (count >= 15) {
                 return@forEach
             }
             try {
                 val user = ctx.jda.retrieveUserById(u.getString("_id")).submit().await()
+                if (user.name.contains("Deleted User")) {
+                    return@forEach
+                }
                 if (user.isBot) {
                     BoobBot.database.getAllUsers().deleteOne(Filters.eq("_id", u["_id"]))
                     return@forEach
                 }
-                msg += "$count: ***${user.name}***  balance: $***${u["balance"]}***\n"
+                msg += "${count + 1}: ***${user.name}***  balance: $***${u["balance"]}***\n"
                 count++
             } catch (e: Exception) {
                 BoobBot.database.getAllUsers().deleteOne(Filters.eq("_id", u["_id"]))
                 return@forEach
             }
-
-            ctx.embed {
-                setAuthor("Global economy leaderboard \uD83C\uDFC6", null, ctx.selfUser.avatarUrl)
-                addField("", msg, false)
-                setFooter("Requested by ${ctx.author.name}", ctx.author.effectiveAvatarUrl)
+            if (count == 15) {
+                return ctx.embed {
+                    setAuthor("Global economy leaderboard \uD83C\uDFC6", null, ctx.selfUser.avatarUrl)
+                    addField("", msg, false)
+                    setFooter("Requested by ${ctx.author.name}", ctx.author.effectiveAvatarUrl)
+                }
             }
         }
     }
 
-    @SubCommand(aliases = ["exp", "xp", "rank"], description = "Global rank leaderboard \uD83C\uDFC6")
+    @SubCommand(aliases = ["exp", "xp", "rank"], description = "Global rank leaderboard \uD83C\uDFC6", async = true)
     suspend fun level(ctx: Context) {
         var msg = ""
         var count = 0
-        BoobBot.database.getAllUsers().find().sort(BasicDBObject("level", -1)).limit(20).iterator().forEach { u ->
+        BoobBot.database.getAllUsers().find().sort(BasicDBObject("level", -1)).limit(25).iterator().forEach { u ->
             if (count >= 15) {
                 return@forEach
             }
             try {
                 val user = ctx.jda.retrieveUserById(u.getString("_id")).submit().await()
+                if (user.name.contains("Deleted User")) {
+                    return@forEach
+                }
                 if (user.isBot) {
                     BoobBot.database.getAllUsers().deleteOne(Filters.eq("_id", u["_id"]))
                     return@forEach
                 }
-                msg += "$count: ***${user.name}***  level: $***${u["level"]}***\n"
+                msg += "***${count + 1}:*** ${user.name} ***level:*** ${u["level"]}\n"
                 count++
             } catch (e: Exception) {
                 BoobBot.database.getAllUsers().deleteOne(Filters.eq("_id", u["_id"]))
                 return@forEach
             }
         }
-        ctx.embed {
-            setAuthor("Global rank leaderboard \uD83C\uDFC6", null, ctx.selfUser.avatarUrl)
-            addField("", msg, false)
-            setFooter("Requested by ${ctx.author.name}", ctx.author.effectiveAvatarUrl)
+        if (count == 15) {
+            return ctx.embed {
+                setAuthor("Global rank leaderboard \uD83C\uDFC6", null, ctx.selfUser.avatarUrl)
+                addField("", msg, false)
+                setFooter("Requested by ${ctx.author.name}", ctx.author.effectiveAvatarUrl)
+            }
         }
     }
 
-    @SubCommand(aliases = ["reputation"], description = "Global reputation leaderboard \uD83C\uDFC6")
+    @SubCommand(aliases = ["reputation"], description = "Global reputation leaderboard \uD83C\uDFC6", async = true)
     suspend fun rep(ctx: Context) {
         var msg = ""
         var count = 0
-        BoobBot.database.getAllUsers().find().sort(BasicDBObject("rep", -1)).limit(15).iterator().forEach { u ->
+        BoobBot.database.getAllUsers().find().sort(BasicDBObject("rep", -1)).limit(25).iterator().forEach { u ->
             if (count >= 15) {
                 return@forEach
             }
             try {
                 val user = ctx.jda.retrieveUserById(u.getString("_id")).submit().await()
+
+                if (user.name.contains("Deleted User")) {
+                    return@forEach
+                }
+
                 if (user.isBot) {
                     BoobBot.database.getAllUsers().deleteOne(Filters.eq("_id", u["_id"]))
                     return@forEach
                 }
-                msg += "$count: ***${user.name}***  rep: ***${u["rep"]}***\n"
+                msg += "${count + 1}: ***${user.name}***  rep: ***${u["rep"]}***\n"
                 count++
             } catch (e: Exception) {
                 BoobBot.database.getAllUsers().deleteOne(Filters.eq("_id", u["_id"]))
                 return@forEach
             }
-
-            ctx.embed {
-                setAuthor("Global reputation leaderboard \uD83C\uDFC6", null, ctx.selfUser.avatarUrl)
-                addField("", msg, false)
-                setFooter("Requested by ${ctx.author.name}", ctx.author.effectiveAvatarUrl)
+            if (count == 15) {
+                return ctx.embed {
+                    setAuthor("Global reputation leaderboard \uD83C\uDFC6", null, ctx.selfUser.avatarUrl)
+                    addField("", msg, false)
+                    setFooter("Requested by ${ctx.author.name}", ctx.author.effectiveAvatarUrl)
+                }
             }
         }
 
