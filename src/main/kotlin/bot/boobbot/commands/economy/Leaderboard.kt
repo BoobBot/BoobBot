fe0f5b170322d9f2b2cb19e65628b3a2a6df4940
IamTails <tom@boob.bot>
2020-09-07 15:24:18 -0400

    async lb

--- src/main/kotlin/bot/boobbot/commands/economy/Leaderboard.kt
+++ src/main/kotlin/bot/boobbot/commands/economy/Leaderboard.kt
@@ -18,75 +18,42 @@ class Leaderboard : AsyncCommand {
 
     @SubCommand(aliases = ["richest", "$"], description = "Global economy leaderboard \uD83C\uDFC6", async = true)
     suspend fun cash(ctx: Context) {
-        var msg = ""
-        var count = 0
-        BoobBot.database.getAllUsers().find().sort(BasicDBObject("balance", -1)).limit(25).iterator().forEach { u ->
-            if (count >= 15) {
-                return@forEach
-            }
-            try {
-                val user = ctx.jda.retrieveUserById(u.getString("_id")).submit().await()
-                if (user.name.contains("Deleted User")) {
-                    return@forEach
-                }
-                if (user.isBot) {
-                    BoobBot.database.getAllUsers().deleteOne(Filters.eq("_id", u["_id"]))
-                    return@forEach
-                }
-                msg += "${count + 1}: ***${user.name}***  balance: $***${u["balance"]}***\n"
-                count++
-            } catch (e: Exception) {
-                BoobBot.database.getAllUsers().deleteOne(Filters.eq("_id", u["_id"]))
-                return@forEach
-            }
-            if (count == 15) {
-                return ctx.embed {
+        val msg = buildMessage("balance", ctx)
+                 ctx.embed {
                     setAuthor("Global economy leaderboard \uD83C\uDFC6", null, ctx.selfUser.avatarUrl)
                     addField("", msg, false)
                     setFooter("Requested by ${ctx.author.name}", ctx.author.effectiveAvatarUrl)
                 }
             }
-        }
-    }
+
+
 
     @SubCommand(aliases = ["exp", "xp", "rank"], description = "Global rank leaderboard \uD83C\uDFC6", async = true)
     suspend fun level(ctx: Context) {
-        var msg = ""
-        var count = 0
-        BoobBot.database.getAllUsers().find().sort(BasicDBObject("level", -1)).limit(25).iterator().forEach { u ->
-            if (count >= 15) {
-                return@forEach
-            }
-            try {
-                val user = ctx.jda.retrieveUserById(u.getString("_id")).submit().await()
-                if (user.name.contains("Deleted User")) {
-                    return@forEach
-                }
-                if (user.isBot) {
-                    BoobBot.database.getAllUsers().deleteOne(Filters.eq("_id", u["_id"]))
-                    return@forEach
-                }
-                msg += "***${count + 1}:*** ${user.name} ***level:*** ${u["level"]}\n"
-                count++
-            } catch (e: Exception) {
-                BoobBot.database.getAllUsers().deleteOne(Filters.eq("_id", u["_id"]))
-                return@forEach
-            }
-        }
-        if (count == 15) {
-            return ctx.embed {
+        val msg = buildMessage("level", ctx)
+            ctx.embed {
                 setAuthor("Global rank leaderboard \uD83C\uDFC6", null, ctx.selfUser.avatarUrl)
                 addField("", msg, false)
                 setFooter("Requested by ${ctx.author.name}", ctx.author.effectiveAvatarUrl)
             }
-        }
     }
 
     @SubCommand(aliases = ["reputation"], description = "Global reputation leaderboard \uD83C\uDFC6", async = true)
     suspend fun rep(ctx: Context) {
+        val msg = buildMessage("rep", ctx)
+                ctx.embed {
+                    setAuthor("Global reputation leaderboard \uD83C\uDFC6", null, ctx.selfUser.avatarUrl)
+                    addField("", msg, false)
+                    setFooter("Requested by ${ctx.author.name}", ctx.author.effectiveAvatarUrl)
+                }
+        }
+
+
+    suspend fun buildMessage(key: String, ctx: Context): String {
         var msg = ""
         var count = 0
-        BoobBot.database.getAllUsers().find().sort(BasicDBObject("rep", -1)).limit(25).iterator().forEach { u ->
+        BoobBot.database.getAllUsers().find().sort(BasicDBObject(key, -1)).limit(25).iterator().forEach { u ->
+
             if (count >= 15) {
                 return@forEach
             }
@@ -101,21 +68,16 @@ class Leaderboard : AsyncCommand {
                     BoobBot.database.getAllUsers().deleteOne(Filters.eq("_id", u["_id"]))
                     return@forEach
                 }
-                msg += "${count + 1}: ***${user.name}***  rep: ***${u["rep"]}***\n"
+                val label = if (key.contains("balance")) {"$key: $"} else "$key: "
+                msg += "${count + 1}:  ***${user.name}***   $label***${u[key]}***\n"
                 count++
             } catch (e: Exception) {
                 BoobBot.database.getAllUsers().deleteOne(Filters.eq("_id", u["_id"]))
                 return@forEach
             }
-            if (count == 15) {
-                return ctx.embed {
-                    setAuthor("Global reputation leaderboard \uD83C\uDFC6", null, ctx.selfUser.avatarUrl)
-                    addField("", msg, false)
-                    setFooter("Requested by ${ctx.author.name}", ctx.author.effectiveAvatarUrl)
-                }
-            }
         }
+
+        return msg
 
     }
 }
-
