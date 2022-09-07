cf840366e6dcd35b6b52c28ba4029ef5ff7f30a6
Devoxin <lukel9914@gmail.com>
2022-09-07 18:41:29 +0100

    Clean

--- src/main/kotlin/bot/boobbot/commands/bot/Clean.kt
+++ src/main/kotlin/bot/boobbot/commands/bot/Clean.kt
@@ -2,11 +2,15 @@ package bot.boobbot.commands.bot
 
 import bot.boobbot.BoobBot
 import bot.boobbot.entities.framework.Category
+import bot.boobbot.entities.framework.Context
 import bot.boobbot.entities.framework.interfaces.Command
 import bot.boobbot.entities.framework.annotations.CommandProperties
 import bot.boobbot.entities.framework.MessageContext
+import bot.boobbot.entities.framework.annotations.Option
+import bot.boobbot.entities.framework.impl.Resolver
 import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.interactions.commands.OptionType
 import java.util.concurrent.TimeUnit
 
 
@@ -16,9 +20,10 @@ import java.util.concurrent.TimeUnit
     guildOnly = true,
     category = Category.MISC
 )
+@Option("amount", description = "The maximum number of messages to remove. Limit of 500.", type = OptionType.INTEGER)
 class Clean : Command {
-    private fun filterDeletable(ctx: MessageContext, messages: List<Message>): List<Message> {
-        val selfId = ctx.jda.selfUser.idLong
+    private fun filterDeletable(ctx: Context, messages: List<Message>): List<Message> {
+        val selfId = ctx.selfUser.idLong
         val prefixes = mutableListOf(
             BoobBot.defaultPrefix,
             "<@$selfId>",
@@ -30,7 +35,7 @@ class Clean : Command {
         }
     }
 
-    override fun execute(ctx: MessageContext) {
+    override fun execute(ctx: Context) {
         if (!ctx.botCan(Permission.MESSAGE_MANAGE)) {
             return ctx.reply("\uD83D\uDEAB Hey whore, I lack the `MANAGE_MESSAGES` permission needed to do this")
         }
@@ -43,8 +48,10 @@ class Clean : Command {
             return ctx.reply("\uD83D\uDEAB Hey whore, you lack the `MANAGE_MESSAGES` permission needed to do this")
         }
 
+        val amount = ctx.options.getByNameOrNext("amount", Resolver.INTEGER) ?: 100
+
         ctx.channel.iterableHistory
-            .takeAsync(100)
+            .takeAsync(amount)
             .thenApply { filterDeletable(ctx, it) }
             .thenApply { ctx.channel.purgeMessages(it) }
             .thenAccept { ctx.reply("Channel cleaned, whore. Want me to cook you dinner too?", true) }
