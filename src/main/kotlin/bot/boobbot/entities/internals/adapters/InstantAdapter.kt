a8f85a502053b804514584e3c9791ad82c75a0cc
devoxin <lukel9914@gmail.com>
2025-07-20 01:07:54 +0100

    instant is serialized as an object

--- src/main/kotlin/bot/boobbot/entities/internals/adapters/InstantAdapter.kt
+++ src/main/kotlin/bot/boobbot/entities/internals/adapters/InstantAdapter.kt
@@ -3,6 +3,7 @@ package bot.boobbot.entities.internals.adapters
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
+import com.google.gson.JsonObject
 import com.google.gson.JsonPrimitive
 import com.google.gson.JsonSerializationContext
 import com.google.gson.JsonSerializer
@@ -11,10 +12,32 @@ import java.time.Instant
 
 class InstantAdapter : JsonSerializer<Instant>, JsonDeserializer<Instant> {
     override fun serialize(instant: Instant, type: Type, context: JsonSerializationContext): JsonElement {
-        return JsonPrimitive(instant.toEpochMilli())
+        val json = JsonObject()
+
+        json.add("seconds", JsonPrimitive(instant.epochSecond))
+        json.add("nanos", JsonPrimitive(instant.nano))
+
+        return json
     }
 
     override fun deserialize(element: JsonElement, type: Type, context: JsonDeserializationContext?): Instant {
-        return Instant.ofEpochMilli(element.asLong)
+        return when {
+            element.isJsonPrimitive -> Instant.ofEpochMilli(element.asLong)
+            element.isJsonObject -> {
+                val seconds = element.asJsonObject.get("seconds")
+                val nanos = element.asJsonObject.get("nanos")
+
+                if (seconds.isJsonPrimitive) {
+                    if (nanos.isJsonPrimitive) {
+                        return Instant.ofEpochSecond(seconds.asLong, nanos.asLong)
+                    }
+
+                    return Instant.ofEpochSecond(seconds.asLong)
+                }
+
+                throw UnsupportedOperationException()
+            }
+            else -> throw UnsupportedOperationException()
+        }
     }
 }
